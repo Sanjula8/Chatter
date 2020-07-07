d264bc88e8e392ea264767df9421a08ad2eb472b
Sanjula <sanjula8@gmail.com>
2020-07-07 14:59:09 -0500

    added middleware auth for authentication and authorization

--- chatter-server/middleware/auth.js
+++ chatter-server/middleware/auth.js
@@ -1,4 +1,4 @@
-require("dotenv").load();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 // Make sure User is logged in - Authentication
@@ -26,4 +26,23 @@ exports.loginRequired = function (req, res, next) {
 
 // Make sure to get the correct user - Authorization
 
-exports.ensureCorrectUser = function (req, res, next) {};
+exports.ensureCorrectUser = function (req, res, next) {
+	try {
+		const token = req.headers.authorization.split(" ")[1];
+		jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+			if (decoded && decoded.id === req.params.id) {
+				return next();
+			} else {
+				return next({
+					status: 401,
+					message: "Unauthorized",
+				});
+			}
+		});
+	} catch (err) {
+		return next({
+			status: 401,
+			message: "Unauthorized",
+		});
+	}
+};
