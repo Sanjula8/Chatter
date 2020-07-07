53b61de85bc4dd16aab9d82079935b7a0c7c6fd1
Sanjula <sanjula8@gmail.com>
2020-07-07 11:23:33 -0500

    writing index.js file

--- chatter-server/index.js
+++ chatter-server/index.js
@@ -6,3 +6,15 @@ const cors = require('cors');
 const PORT = 8081;
 app.use(cors());
 app.use(bodyParser.json());
+
+// All Routes Here
+
+app.use(function (req, res, next) {
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.listen(PORT, function () {
+	console.log(`Server is starting on Port ${PORT}`);
+});
